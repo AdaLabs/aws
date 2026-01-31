daa6902dc4037ee9927009ff60c13f5a67e2334d
David SAUVAGE - AdaLabs Ltd <david.sauvage@adalabs.com>
2026-01-31 23:58:54 +0400

    #256844, Integration AWS Commit c4bbd40 - Spaces and TAB must be removed from header lines.

--- src/core/aws-headers.adb
+++ src/core/aws-headers.adb
@@ -218,6 +218,9 @@ package body AWS.Headers is
 
       procedure Parse_Header_Line (Line : String) is
          use Ada.Strings;
+         OWS             : constant Strings.Maps.Character_Set :=
+                             Strings.Maps.To_Set (" " & ASCII.HT);
+         --  OWS is defined in RFC 9110 section 5.6.3
          Delimiter_Index : Natural;
       begin
          if Debug_Flag then
@@ -243,8 +246,9 @@ package body AWS.Headers is
          Add (Headers,
            Name  => Line (Line'First .. Delimiter_Index - 1),
            Value => Fixed.Trim
-             (Line (Delimiter_Index + 1 .. Line'Last),
-              Side => Both));
+                      (Line (Delimiter_Index + 1 .. Line'Last),
+                       Left  => OWS,
+                       Right => OWS));
       end Parse_Header_Line;
 
       End_Of_Message : constant String := "";
